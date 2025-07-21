7a895132875af4e06090260c0f17ec17c9175098
amirkateb <amveks43@gmail.com>
2025-07-21 06:11:21 +0330

    Update smart-traffic.sh

--- smart-traffic.sh
+++ smart-traffic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if vnstat is installed
+
 if ! command -v vnstat &>/dev/null; then
   echo "📦 Installing vnstat..."
   sudo apt update -y && sudo apt install -y vnstat
@@ -9,6 +9,3 @@ fi
 echo "📈 Network Traffic (5 seconds sample)"
 echo "====================================="
 vnstat -tr 5
-
-echo -e "\n\033[1;33m✨ Press any key to return to main menu...\033[0m"
-read -n 1 -s
