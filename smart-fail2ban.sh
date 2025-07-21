6110e709a7722d06c1093109f68e6e67c70db98e
amirkateb <amveks43@gmail.com>
2025-07-21 06:07:57 +0330

    Update smart-fail2ban.sh

--- smart-fail2ban.sh
+++ smart-fail2ban.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check and install fail2ban if not installed
+
 if ! command -v fail2ban-client &>/dev/null; then
   echo "📦 Installing fail2ban..."
   sudo apt update -y && sudo apt install -y fail2ban
@@ -10,5 +10,3 @@ echo "🔐 Fail2Ban Status"
 echo "=================="
 sudo systemctl enable --now fail2ban
 sudo fail2ban-client status
-echo -e "\n\033[1;33m✨ Press any key to return to main menu...\033[0m"
-read -n 1 -s
