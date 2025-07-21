800ba3a71a82a1e2eaad20613f32ae40e6a0b370
amirkateb <amveks43@gmail.com>
2025-07-21 05:00:58 +0330

    Update smart-status.sh

--- smart-status.sh
+++ smart-status.sh
@@ -17,3 +17,5 @@ df -h /
 echo ""
 echo "📦 Top 5 Processes by CPU:"
 ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 6
+echo -e "\n\033[1;33m✨ Press any key to return to main menu...\033[0m"
+read -n 1 -s
