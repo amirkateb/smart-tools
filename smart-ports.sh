969d81916ac4b61f993a87572c2c0c22d4dc1272
amirkateb <amveks43@gmail.com>
2025-07-21 04:59:28 +0330

    Update smart-ports.sh

--- smart-ports.sh
+++ smart-ports.sh
@@ -3,3 +3,5 @@
 echo "🔓 Open Listening Ports"
 echo "========================"
 sudo ss -tulnp | grep LISTEN || sudo netstat -tulnp | grep LISTEN
+echo -e "\n\033[1;33m✨ Press any key to return to main menu...\033[0m"
+read -n 1 -s
